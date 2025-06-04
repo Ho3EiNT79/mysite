9ef0c1b2a6bf9fba8fb74090f42c841466a1e7d7
Ho3EiNT79 <hosseinj456@gmail.com>
2025-06-04 14:58:34 +0330

    Update models and templates to reflect latest changes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    author = models.ForeignKey(User, models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # Tag
+    # Category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_date"]
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
 
+
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context=context)
 
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(
+        Post, id=pid, status=True, published_date__lte=timezone.now()
+    )
+    
+    post.counted_views += 1
+    post.save(update_fields=["counted_views"])
+    
+    context = {"post": post}
+    return render(request, "blog/blog-single.html", context=context)
+    
